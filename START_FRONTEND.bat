8ab9b49abcdba4ad62c4d4e425556eef99ad1e8e
Legendairy107 <youssefeldairy2005@gmail.com>
2026-06-20 17:26:16 +0200

    Delete START_FRONTEND.bat

--- START_FRONTEND.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-title PopEyez Frontend
-cd /d "%~dp0frontend"
-echo Installing frontend dependencies...
-call npm install
-echo Starting frontend. Open the Vite URL shown below, usually http://localhost:5173 ...
-call npm run dev
-pause
